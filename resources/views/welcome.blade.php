46630b8f0bfa5ec642cd6eced1c7c1238737b44d
QuitaMoz <108766495+Labrador-Antonio@users.noreply.github.com>
2023-08-08 20:11:26 -0700

    titulo de landingpage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
+    <title>Bienvenido | EcoMeter </title>
     <link href="{{ asset('assets/css/home.css') }}" rel="stylesheet" type="text/css"/>
     <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css"/>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" type="text/css" />
